84a9ec483f2a0c44e829bd9522c38a31889ccfd4
radoman2 <radoman88@gmail.com>
2024-08-14 17:26:41 +0200

    add post method to main.py

--- nlp_service/main.py
+++ nlp_service/main.py
@@ -5,17 +5,17 @@ import torch
 import requests
 from torchtext.vocab import GloVe
 from tensorflow.keras.models import load_model
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import uvicorn
 
 # Load GloVe embeddings
 glove = GloVe(name="6B", dim=50) # load glove embeddings
 
-response = requests.get("http://127.0.0.1:8000/") # fetch json data from a server
-questions_json = response.json()
+app = FastAPI() # create FastAPI app
 
-questions = pd.DataFrame(questions_json) # convert json to datafram
+# response = requests.get("http://127.0.0.1:8000/") # fetch json data from a server
+
 
 def preprocess_question(question, glove): # first we need to preprocess the questions
     tokens = question.lower().split()
@@ -25,58 +25,66 @@ def preprocess_question(question, glove): # first we need to preprocess the ques
     vector = torch.mean(torch.stack(vectors), dim=0).numpy()
     return vector
 
-vectors = []
-for index, question in questions['q'].items():
-    question_vector = preprocess_question(question, glove)
-    question_vector = np.expand_dims(question_vector, axis=0) # reshape to match the input expected by the model
-    vectors.append(question_vector)
+@app.post("/process-questions/")
+async def process_questions(request: Request):
+    questions_json = await request.json()  # Receive the JSON data
+    questions = pd.DataFrame(questions_json)  # convert JSON to DataFrame
 
-questions['vector'] = vectors
-users_question = questions.iloc[[-1]]
-# print(users_question)
+    vectors = []
+    for index, question in questions['q'].items():
+        question_vector = preprocess_question(question, glove)
+        question_vector = np.expand_dims(question_vector, axis=0) # reshape to match the input expected by the model
+        vectors.append(question_vector)
 
-questions = questions.drop(questions.index[-1])
-# print(questions)
+    questions['vector'] = vectors
+    users_question = questions.iloc[[-1]]
+    # print(users_question)
 
-users_question = questions.iloc[[-1]]
+    questions = questions.drop(questions.index[-1])
+    # print(questions)
 
-
-siamese_network = load_model('model/siamese_model.h5') # load the model
-
-similarity_scores = []
-user_vector = users_question['vector'].values[0]
-
-for vector in questions['vector']:
-    vector = np.squeeze(vector)
-    user_vector = np.squeeze(user_vector)
-    
-    similarity_score = siamese_network.predict([np.expand_dims(vector, axis=0), np.expand_dims(user_vector, axis=0)]) # compute similarity
-    similarity_scores.append(similarity_score[0][0])  # append to list
+    users_question = questions.iloc[[-1]]
 
 
-questions['similarity_score'] = similarity_scores # add column
+    siamese_network = load_model('model/siamese_model.h5') # load the model
 
-# print(questions)
+    similarity_scores = []
+    user_vector = users_question['vector'].values[0]
 
-questions_sorted = questions.sort_values(by='similarity_score', ascending=False)
-# print(questions_sorted)
-# print(questions_sorted.iloc[0]) 
-# print(questions_sorted.iloc[0][['q', 'a']]) 
+    for vector in questions['vector']:
+        vector = np.squeeze(vector)
+        user_vector = np.squeeze(user_vector)
+        
+        similarity_score = siamese_network.predict([np.expand_dims(vector, axis=0), np.expand_dims(user_vector, axis=0)]) # compute similarity
+        similarity_scores.append(similarity_score[0][0])  # append to list
 
-best_question_answer = questions_sorted.iloc[0][['q', 'a']].to_dict()
-# print(best_question_answer)
+
+    questions['similarity_score'] = similarity_scores # add column
+
+    # print(questions)
+
+    questions_sorted = questions.sort_values(by='similarity_score', ascending=False)
+    # print(questions_sorted)
+    # print(questions_sorted.iloc[0]) 
+    # print(questions_sorted.iloc[0][['q', 'a']]) 
+
+    best_question_answer = questions_sorted.iloc[0][['q', 'a']].to_dict()
+    # print(best_question_answer)
+    return JSONResponse(content=best_question_answer)  # Return the best question and answer
 
 
-# print(best_question_answer)
-# PREVIOUS SOLUTION -> sent asnwer to a .json file
-# with open('output/best_question_answer.json', 'w') as json_file:
-#     json.dump(best_question_answer, json_file, indent=4)
+    # print(best_question_answer)
+    # PREVIOUS SOLUTION -> sent asnwer to a .json file
+    # with open('output/best_question_answer.json', 'w') as json_file:
+    #     json.dump(best_question_answer, json_file, indent=4)
 
-app = FastAPI() # create FastAPI app
-
-@app.get("/")
-def get_best_question_answer():
-    return JSONResponse(content=best_question_answer) # prepare json
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080) # send best_question_answer json to port 8080
+    uvicorn.run(app, host="127.0.0.1", port=8000)  # Run the FastAPI server on port 3000
+
+# @app.get("/")
+# def get_best_question_answer():
+#     return JSONResponse(content=best_question_answer) # prepare json
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8080) # send best_question_answer json to port 8080
